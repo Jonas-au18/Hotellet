cd464ff460af2ce8b91c7df6e005bedf65581e79
Victor Desktop <Victo@DESKTOP-C0LLRRO>
2020-04-20 15:48:59 +0200

    added 1. layer of design

---
+++ Hotel_california/Data/Migrations/20200420133419_viggo.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Hotel_california.Data.Migrations
+{
+    public partial class viggo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
