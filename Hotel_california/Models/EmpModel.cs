7fc5f67cfdf0eade3e6dcb614e6387a709d49826
Victor Desktop <Victo@DESKTOP-C0LLRRO>
2020-04-20 18:34:54 +0200

    made a test 2 with a somewhat working calender

---
+++ Hotel_california/Models/EmpModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Hotel_california.Models
+{
+    public class EmpModel
+    {
+        /// <summary>  
+        /// DOB datetime data type property   
+        /// to display date type control  
+        /// </summary>  
+        [Display(Name = "Date of Birth")]
+        [DataType(DataType.Date)]
+        public DateTime? DOB { get; set; }
+    }
+}
